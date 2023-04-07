ebc1e0f620b510a612c5106d8706b090fe63aaa6
DavidBarros <DavidB4rros@gmail.com>
2023-04-07 15:25:08 -0300

    Initial Commit

---
+++ src/components/calculator.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './calculator.css'
+
+export default class Calcuator extends Component{
+
+    render(){
+       return( 
+        <div className = "calculator">
+            
+        </div>
+       )
+    }
+
+
+
+
+
+}
